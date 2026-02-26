a379ff4e49a0c92a00c6f689a2a2655328b4505a
Max <maximefremovse@gmail.com>
2026-02-26 14:21:56 -0600

    Initial Commit

---
+++ RateLimited/RateLimitedApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateLimitedApp.swift
+//  RateLimited
+//
+//  Created by Max on 2/26/26.
+//
+
+import SwiftUI
+
+@main
+struct RateLimitedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
